d93a237749dac196b7f5f37cc505b823142616fe
NoahRGB <ncox2004@gmail.com>
2025-11-23 13:16:23 +0000

    bandit class

--- bandits.py
+++ bandits.py
@@ -3,7 +3,7 @@ import math
 
 import matplotlib.pyplot as plt
 
-from bandit import Bandit
+from bandit import KArmedBandit
 
 def argmax(d):
     # returns a random key in the dict among those that have the max value
@@ -13,14 +13,13 @@ def argmax(d):
 
 def plot(x, y_vals, y_labels=[], x_title="", y_title="", y_lims=None, x_lims=None):
     for i in range(0, len(y_vals)):
-        plt.plot(x, y_vals[i], label=y_labels[i])
+        plt.plot(x, y_vals[i], label=y_labels[i] if len(y_labels) > i else "")
     plt.legend()
     if y_lims: plt.ylim(y_lims[0], y_lims[1])
     if x_lims: plt.xlim(x_lims[0], x_lims[1])
     plt.xlabel(x_title)
     plt.ylabel(y_title)
     plt.show()
-
 
 def k_armed_bandits(k, steps, epsilon, quiet=True, Q1=0, a=None):
     # returns the average reward recieved over steps and the percentage
@@ -43,7 +42,6 @@ def k_armed_bandits(k, steps, epsilon, quiet=True, Q1=0, a=None):
         R.append(random.gauss(q[A], 1))
         N[A] += 1
 
-        
         if not a: # Q[A] is updated using sample averages
             Q[A].append(Q[A][-1] + ((R[i] - Q[A][-1]) / N[A]))
         else:
@@ -75,11 +73,39 @@ def run_bandit_trials(trials, k, epsilons, min_step=100, max_step=1000, step_ste
     plot(step_range, list(epsilon_reward.values()), [f"ε=0.0", f"ε=0.01", f"ε=0.1"], "Step sizes", "Average reward recieved")
     plot(step_range, list(epsilon_optimal_count.values()), [f"ε=0.0", f"ε=0.01", f"ε=0.1"], "Step sizes", "Percentage of the time the optimal action was chosen", y_lims=[0, 100])
 
+def run_bandit_trials2(trials, k, epsilons, min_step=100, max_step=1000, step_step=100, quiet=True):
+    epsilon_average_rewards = {epsilon: [] for epsilon in epsilons}
+    epsilon_average_optimal_percentage = {epsilon: [] for epsilon in epsilons}
+    step_range = [1] + [i for i in range(min_step, max_step+1, step_step)]
+    if min_step == max_step == 1: step_range = [1]
+    for epsilon in epsilons:
+        for steps in step_range:
+            if not quiet: print(f"ε = {epsilon}, steps = {steps}")
+            bandit = KArmedBandit(10, 0, epsilon)
+            for i in range(0, trials):
+                bandit.run()
+            epsilon_average_rewards[epsilon]
+
+                
+
+
 
 if __name__ == "__main__":
     #run_bandit_trials(2000, 10, [0.0, 0.01, 0.1], quiet=False)
     #run_bandit_trials(10000, 10, [0.1], quiet=False)
     #run_bandit_trials(1, 10, [0.1], quiet=False, min_step=1, max_step=5, step_step=1)
-
-    b1 = Bandit(random.gauss(1, 0), 0)
+    trials = 2000
+    avg_rewards = []
+    step_range = [1] + [i for i in range(100, 1001, 100)]
+    for steps in step_range:
+        print(f"Running bandit with steps = {steps}")
+        avg_reward = 0
+        for i in range(0, trials):
+            bandit = KArmedBandit(10, 0, 0.1)
+            for i in range(0, steps):
+                bandit.run()
+            avg_reward += sum(bandit.rewards) / steps
+        avg_rewards.append(avg_reward / trials)
+    print(avg_rewards)
+    plot(step_range, [avg_rewards])
     
